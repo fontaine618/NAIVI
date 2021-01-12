fdbcc344c510d2b65583b3435c4b55f4f58bcad6
simon <simfont@umich.edu>
2021-01-12 09:55:33 -0500

    binary experiments networksize

---
+++ sims/networksize_binary/job_mle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=nnvi_networksize_binary_mle
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=100:00:00
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=12g
+#SBATCH --output=/home/%u/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+source /home/simfont/nnvi/bin/activate
+python3 main_mle.py
+
---
+++ sims/networksize_binary/main_vimc.py
@@ -0,0 +1,24 @@
+import sys
+PATH = "/home/simfont/NNVI/"
+sys.path.append(PATH)
+import numpy as np
+import torch
+from sims.main import main
+
+if __name__ == "__main__":
+    torch.set_default_dtype(torch.float64)
+    main(
+        path=PATH,
+        name="networksize_binary_vimc",
+        explore_dict={
+            "data.N": np.array([50, 100, 200, 500, 1000, 2000]),
+            "data.K": np.array([5]),
+            "data.p_cts": np.array([0]),
+            "data.p_bin": np.array([10, 100, 500]),
+            "data.missing_rate": np.array([0.10]),
+            "data.seed": np.arange(0, 10, 1),
+            "data.alpha_mean": np.array([-1.85]),
+            "model.K": np.array([5]),
+            "fit.algo": ["VIMC"]
+        }
+    )
