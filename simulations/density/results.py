d83b3daa2e364a0d2a3027ca292ed53f00608d2d
simon <simfont@umich.edu>
2021-05-05 21:12:21 -0400

    more cleanup

--- simulations/density/results.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-SIM_NAME = "density"
-SIM_PATH = "/home/simon/Documents/NAIVI/simulations/" + SIM_NAME
-
-results = pd.read_csv(SIM_PATH + "/results/summary.csv", index_col=0)
-
-results.groupby("alpha_mean").agg("mean").to_csv(SIM_PATH + "/results/density.csv")
