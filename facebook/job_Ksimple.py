f5a0e9dacef5dc0d867c4cc8fe094f4f6040310a
simon <simfont@umich.edu>
2021-05-16 17:00:52 -0400

    facebook simple exp

--- facebook/job_Ksimple.py
+++ facebook/job_Ksimple.py
@@ -6,19 +6,16 @@ import torch
 from facebook.main import main
 from facebook.data import get_centers
 
-
-centers = get_centers(PATH + "data/raw/")
-
 if __name__ == "__main__":
     torch.set_default_dtype(torch.float64)
     main(
         path=PATH,
         name="Kfb_simple",
         explore_dict={
-            "data.missing_rate": np.array([0.25, 0.50]),
+            "data.missing_rate": np.array([0.50]),
             "data.seed": np.arange(0, 10, 1),
-            "data.center": np.array(centers),
-            "model.K": np.array([5]),
+            "data.center": np.array([698, 0, 1684]),
+            "model.K": np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20]),
             "fit.algo": ["NetworkSmoothing", "Mean", "MissForest"],
             "fit.max_iter": np.array([1000])
         }
