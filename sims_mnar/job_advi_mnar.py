3d9950a632147ab42081843c32471ec73ed7d038
simon <simfont@umich.edu>
2021-06-14 21:40:24 -0400

    cts mnar experiments

--- sims_mnar/job_advi_mnar.py
+++ sims_mnar/job_advi_mnar.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     torch.set_default_dtype(torch.float64)
     main(
         path=PATH,
-        name="mnar_advi_mnar",
+        name="mnar_cts_advi_mnar",
         explore_dict={
             "data.N": np.array([500]),
             "data.K": np.array([5]),
-            "data.p_bin": np.array([100]),
-            "data.p_cts": np.array([0]),
+            "data.p_bin": np.array([0]),
+            "data.p_cts": np.array([100]),
             "data.missing_mean": np.linspace(-3., 1., 13),
             "data.seed": np.arange(0, 10, 1),
             "data.alpha_mean": np.array([-1.85]),
