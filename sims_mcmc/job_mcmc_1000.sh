f0cb16cb3721b162c5d1765d42f57c58a57e29bc
simon <simfont@umich.edu>
2021-10-13 04:02:02 -0400

    more memory for mcmc 1000 job

--- sims_mcmc/job_mcmc_1000.sh
+++ sims_mcmc/job_mcmc_1000.sh
@@ -10,7 +10,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=100g
+#SBATCH --mem-per-cpu=300g
 #SBATCH --output=/home/%u/%x-%j.log
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
