8418ff0d66aed28603015431f1c7552c94790748
simon <simfont@umich.edu>
2021-10-19 21:32:41 -0400

    n=1000 mcmc experiment

--- sims_mcmc/job_mcmc_1000.py
+++ sims_mcmc/job_mcmc_1000.py
@@ -6,18 +6,29 @@ import numpy as np
 import torch
 from sims_mcmc.main import main
 
+
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        which = int(sys.argv[1])
+        seed = np.array([which % 10])
+        ps = np.array([[0, 50][which // 10]])
+        name = f"mcmc_1000_{which}"
+    else: # no argument = run all
+        seed = np.arange(0, 10, 1)
+        ps = np.array([0, 50])
+        name = "mcmc_1000_all"
+    print(seed, ps)
     torch.set_default_dtype(torch.float64)
     main(
         path=PATH + "sims_mcmc/",
-        name="mcmc_1000",
+        name=name,
         explore_dict={
             "data.N": np.array([1000]),
             "data.K": np.array([2]),
             "data.p_bin": np.array([0]),
-            "data.p_cts": np.array([0, 50]),
+            "data.p_cts": ps,
             "data.missing_mean": np.array([-10000.]),
-            "data.seed": np.arange(0, 10, 1),
+            "data.seed": seed,
             "data.alpha_mean": np.array([-1.85]),
             "data.mnar_sparsity": np.array([0.0]),
             "fit.algo": ["MCMC"],
