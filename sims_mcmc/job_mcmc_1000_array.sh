8d3c97f446ee7733cb93740df9bf0784f6cd2340
simon <simfont@umich.edu>
2021-10-20 22:51:50 -0400

    vi experiments

--- sims_mcmc/job_mcmc_1000_array.sh
+++ sims_mcmc/job_mcmc_1000_array.sh
@@ -4,13 +4,13 @@
 #SBATCH --job-name=naivi_networksize_mcmc_1000
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=10:00:00
+#SBATCH --time=5:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=largemem
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=80g
+#SBATCH --mem-per-cpu=160g
 #SBATCH --output=/home/%u/%x-%j.log
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
