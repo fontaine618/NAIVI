933fd579ec64ef07ca024a49f347a2147cff0a9d
fontaine618 <simfont@umich.edu>
2022-02-12 10:52:25 -0500

    bunch of updates, sorry future simon

---
+++ sims_final/estimation_N_gpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=estimation_N_gpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=01:00:00
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=12g
+#SBATCH --output=/home/%u/scratch/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+source /home/simfont/naivi/bin/activate
+python3 estimation_N_gpu.py
