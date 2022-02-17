4d469dc4fea96a79525874bb17354627aeea4a58
fontaine618 <simfont@umich.edu>
2022-02-16 23:48:26 -0500

    split by seed

--- sims_final/estimation_N_gpu.sh
+++ sims_final/estimation_N_gpu.sh
@@ -4,24 +4,14 @@
 #SBATCH --job-name=estimation_N_gpu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=01:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=12g
 #SBATCH --output=/home/%u/scratch/logs/%x-%A-%a.log
-
-if [$SLURM_ARRAY_TASK_ID / 10 = 0]; then
-  #SBATCH --time=01:00:00
-  #SBATCH --nodes=1
-  #SBATCH --cpus-per-task=1
-  #SBATCH --gpus-per-node=1
-  #SBATCH --mem-per-gpu=12g
-else
-  #SBATCH --time=01:00:00
-  #SBATCH --nodes=1
-  #SBATCH --cpus-per-task=1
-  #SBATCH --gpus-per-node=0
-  #SBATCH --mem-per-cpu=12g
-fi
-
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
 source /home/simfont/naivi/bin/activate
