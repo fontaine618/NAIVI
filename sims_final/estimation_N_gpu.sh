e17ee1a4a3c3a9555a8ab0992a1e9dc80c2dfabc
fontaine618 <simfont@umich.edu>
2022-02-16 22:48:27 -0500

    split by seed

--- sims_final/estimation_N_gpu.sh
+++ sims_final/estimation_N_gpu.sh
@@ -15,4 +15,4 @@
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
 source /home/simfont/naivi/bin/activate
-python estimation_N_gpu.py
+python estimation_N_gpu.py $SLURM_ARRAY_TASK_ID
