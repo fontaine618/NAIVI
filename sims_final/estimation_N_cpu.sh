d06711f60e1eb616297c4182524ed367f3c24ecb
fontaine618 <simfont@umich.edu>
2022-02-18 10:06:32 -0500

    mcmc delete cache

--- sims_final/estimation_N_cpu.sh
+++ sims_final/estimation_N_cpu.sh
@@ -6,11 +6,11 @@
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=24:00:00
 #SBATCH --account=stats_dept1
-#SBATCH --partition=standard
+#SBATCH --partition=largemem
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=5
 #SBATCH --gpus-per-node=0
-#SBATCH --mem-per-cpu=12g
+#SBATCH --mem-per-cpu=60g
 #SBATCH --output=/home/%u/scratch/logs/%x-%A-%a.log
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
