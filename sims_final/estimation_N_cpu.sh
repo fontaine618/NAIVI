1d8018a9123c80f59c9a2c73a186a1bdd5d4a02e
fontaine618 <simfont@umich.edu>
2022-04-14 17:37:26 -0400

    mcmc test

--- sims_final/estimation_N_cpu.sh
+++ sims_final/estimation_N_cpu.sh
@@ -10,7 +10,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=5
 #SBATCH --gpus-per-node=0
-#SBATCH --mem-per-cpu=60g
+#SBATCH --mem-per-cpu=200g
 #SBATCH --output=/home/%u/scratch/logs/%x-%A-%a.log
 # The application(s) to execute along with its input arguments and options:
 module load python/3.8.7 gcc/9.2.0
