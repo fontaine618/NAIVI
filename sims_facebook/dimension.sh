1d6020d5be53d16653ff05806f52d00e7eeb38ef
fontaine618 <simfont@umich.edu>
2022-04-20 01:04:23 -0400

    facebook VIMC

--- sims_facebook/dimension.sh
+++ sims_facebook/dimension.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=fb_dimension
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
