0f9fb13d2552ecaad85c4f4fc47ea687b977d28c
fontaine618 <simfont@umich.edu>
2023-03-20 22:54:17 -0400

    nrep

--- experiments/model_selection/job.sh
+++ experiments/model_selection/job.sh
@@ -5,6 +5,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=1:00:00
+#SBATCH --array=0-1
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
