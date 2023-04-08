f318d69e14a2a2b55b83232530b9920116605fd2
fontaine618 <simfont@umich.edu>
2023-04-08 15:54:16 -0400

    edge_density first test

---
+++ experiments/edge_density/run.py
@@ -0,0 +1,55 @@
+import numpy as np
+import torch
+import sys
+import os
+sys.path.insert(1, '/home/simfont/Documents/NAIVI/')
+from pypet import Environment, cartesian_product
+from pypet_experiments.run import run
+from pypet_experiments.utils import add_parameters
+
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')
+    seed = sys.argv[1]
+    os.makedirs(f"results/", exist_ok=True)
+
+    env = Environment(
+        trajectory="edge_density",
+        filename=f"./results/seed{seed}.hdf5",
+        overwrite_file=True,
+        multiproc=True,
+        ncores=1,
+        use_pool=False,
+    )
+    traj = env.trajectory
+    add_parameters(traj)
+
+    traj.f_explore(cartesian_product({
+        "data.dataset": ["synthetic"],
+        "data.seed": [int(seed)],
+        "data.n_nodes": [500],
+        "data.p_cts": [0],
+        "data.p_bin": [100],
+        "data.latent_dim": [5],
+        "data.latent_variance": [1.],
+        "data.latent_mean": [0.],
+        "data.heterogeneity_mean": [-4., -3.5, -3., -2.5, -2., -1.5, -1., -0.5, 0., 0.5, 1., ],
+        "data.heterogeneity_variance": [1.],
+        "data.cts_noise": [1.],
+        "data.missing_covariate_rate": [0.5],
+        "data.missing_edge_rate": [0.],
+        "data.missing_mechanism": ["row_deletion", "uniform", "triangle"],
+        "model.latent_dim": [5],
+        "model.heterogeneity_prior_mean": [-2.],
+        "model.heterogeneity_prior_variance": [2.],
+        "model.latent_prior_mean": [0.],
+        "model.latent_prior_variance": [1.],
+        "method": ["Mean", "KNN", "VMP", "FA", "MAP", "MLE", "ADVI", "NetworkSmoothing", "Oracle", "MICE"],
+        "fit.vmp.max_iter": [100],
+        "fit.vmp.rel_tol": [1e-5],
+        "fit.vmp.cv_folds": [0],
+    }))
+
+    env.run(run)
+
+
+
