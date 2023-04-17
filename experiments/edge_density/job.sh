71d3f079e8380538323d260b4436f2ec3e510639
fontaine618 <simfont@umich.edu>
2023-04-16 23:10:57 -0400

    update jobs for synthetic experiments

--- experiments/edge_density/job.sh
+++ experiments/edge_density/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=naivi_edge_density
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=2:00:00
+#SBATCH --time=1:00:00
 #SBATCH --array=0-30
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
