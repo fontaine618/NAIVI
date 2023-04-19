a41f969efe65d8472d5b25a0b9cf5b84da1cbf16
fontaine618 <simfont@umich.edu>
2023-04-19 08:47:19 -0400

    facebook experiment all

--- experiments/facebook/job.sh
+++ experiments/facebook/job.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=1:00:00
-#SBATCH --array=0
+#SBATCH --array=1-30
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
