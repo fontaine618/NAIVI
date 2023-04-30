d77f7b1b3fe9c946481189bddd061cec05dc142a
fontaine618 <simfont@umich.edu>
2023-04-30 09:12:30 -0400

    cora selection all

--- experiments/cora_selection/job.sh
+++ experiments/cora_selection/job.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=0:20:00
-#SBATCH --array=0
+#SBATCH --array=0-30
 #SBATCH --account=stats_dept1
 #SBATCH --partition=spgpu
 #SBATCH --nodes=1
