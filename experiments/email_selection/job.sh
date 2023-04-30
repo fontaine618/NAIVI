005519262a8666f606dee02627a3c992db814860
fontaine618 <simfont@umich.edu>
2023-04-30 00:44:46 -0400

    email selection 0

---
+++ experiments/email_selection/job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=naivi_email_selection
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=1:00:00
+#SBATCH --array=0
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/naivi/bin/activate
+python -O run.py $SLURM_ARRAY_TASK_ID
---
+++ experiments/email_selection/run.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+import sys
+import os
+sys.path.insert(1, '/home/simfont/Documents/NAIVI/')
+from pypet import Environment, cartesian_product
+from pypet_experiments.run import run
+from pypet_experiments.utils import add_parameters
+
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')
+    seed = sys.argv[1]
+    os.makedirs(f"results/", exist_ok=True)
+
+    env = Environment(
+        trajectory="email_selection",
+        filename=f"./results/seed{seed}.hdf5",
+        overwrite_file=True,
+        multiproc=True,
+        ncores=1,
+        use_pool=False,
+    )
+    traj = env.trajectory
+    add_parameters(traj)
+
+    traj.f_explore(cartesian_product({
+        "data.dataset": ["email"],
+        "data.path": ["~/Documents/NAIVI/datasets/email/"],
+        "data.seed": [int(seed)],
+        "data.missing_edge_rate": [0.],
+        "data.n_seeds": [1, 3, 5, 10],
+        "model.heterogeneity_prior_mean": [float("nan")],
+        "model.heterogeneity_prior_variance": [1.],
+        "model.latent_prior_mean": [0.],
+        "model.latent_prior_variance": [1.],
+        "model.latent_dim": [2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "method": ["VMP", ],
+        "fit.vmp.max_iter": [100],
+        "fit.vmp.rel_tol": [1e-5],
+        "fit.vmp.cv_folds": [5],
+    }))
+
+    env.run(run)
+
+
+
