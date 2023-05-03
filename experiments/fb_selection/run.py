76dc00722f220aa9d26567053277c95754fd702e
fontaine618 <simfont@umich.edu>
2023-05-02 22:23:17 -0400

    fb selection longer

--- experiments/fb_selection/run.py
+++ experiments/fb_selection/run.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         "model.latent_prior_mean": [0.],
         "model.latent_prior_variance": [1.],
         "method": ["VMP", ],
-        "fit.vmp.max_iter": [100],
+        "fit.vmp.max_iter": [500],
         "fit.vmp.rel_tol": [1e-5],
         "fit.vmp.cv_folds": [5],
     }))
