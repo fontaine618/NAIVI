6e48385a298418ac288fb17f9e1ae2d06acaf98f
fontaine618 <simfont@umich.edu>
2023-05-03 00:54:16 -0400

    ext ELBO K for fb

--- pypet_experiments/data.py
+++ pypet_experiments/data.py
@@ -339,16 +339,16 @@ class Dataset:
         X_bin_missing = torch.where(~M_bin, torch.full_like(X_bin, torch.nan), X_bin)
         X_bin[M_bin] = torch.nan
         best_K = { #center :K
-            3980: 3,
-            698: 4,
-            414: 6,
-            686: 6,
-            348: 7,
-            0: 4,
-            3437: 5,
-            1912: 6,
-            1684: 6,
-            107: 6
+            3980: 2,
+            698: 2,
+            414: 2,
+            686: 2,
+            348: 2,
+            0: 3,
+            3437: 4,
+            1912: 4,
+            1684: 4,
+            107: 5
         }[center]
         return cls(
             edge_index_left=i0,
