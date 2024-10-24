e68961bc145338425a6b460285abc5dc9b49fbe5
fontaine618 <simfont@umich.edu>
2024-10-24 00:34:54 -0400

    cora select

--- experiments/cora_selection/job.sh
+++ experiments/cora_selection/job.sh
@@ -4,13 +4,13 @@
 #SBATCH --job-name=naivi_cora_selection
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=20:00:00
-#SBATCH --array=0
+#SBATCH --time=5:00:00
+#SBATCH --array=0-9
 #SBATCH --account=open
 #SBATCH --partition=open
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8g
 # The application(s) to execute along with its input arguments and options:
 module load anaconda/2023.09
