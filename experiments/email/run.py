b44742c1da5cb93777d1541f43d6571f9f2690f4
fontaine618 <simfont@umich.edu>
2024-10-27 01:06:19 -0400

    run email

--- experiments/email/run.py
+++ experiments/email/run.py
@@ -2,8 +2,8 @@ import numpy as np
 import torch
 import sys
 import os
-# sys.path.insert(1, '/storage/home/spf5519/work/NAIVI/')
-sys.path.insert(1, '/home/simon/Documents/NAIVI/')
+sys.path.insert(1, '/storage/home/spf5519/work/NAIVI/')
+# sys.path.insert(1, '/home/simon/Documents/NAIVI/')
 from pypet import Environment, cartesian_product
 from pypet_experiments.run import run
 from pypet_experiments.utils import add_parameters
@@ -26,20 +26,17 @@ if __name__ == "__main__":
 
     traj.f_explore(cartesian_product({
         "data.dataset": ["email"],
-        "data.path": ["/home/simon/Documents/NAIVI/datasets/email/"],
-        # "data.path": ["~/work/NAIVI/datasets/email/"],
+        # "data.path": ["/home/simon/Documents/NAIVI/datasets/email/"],
+        "data.path": ["~/work/NAIVI/datasets/email/"],
         "data.seed": [int(seed)],
         "data.n_seeds": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        "model.latent_dim": [5],
+        "model.latent_dim": [7],
         "model.heterogeneity_prior_mean": [float("nan")],
         "model.heterogeneity_prior_variance": [float("nan")],
         "model.latent_prior_mean": [0.],
         "model.latent_prior_variance": [1.],
         "method": ["Mean", "KNN",  "VMP", "VMP0", "FA", "MAP", "MLE", "NetworkSmoothing", "MICE", "GCN"],
-        "fit.mice.max_iter": [10],
-        "fit.vmp.max_iter": [200],
-        "fit.vmp.min_iter": [20],
-        "fit.vmp.damping": [0.7],
+        "fit.vmp.max_iter": [2000],
     }))
 
     env.run(run)
