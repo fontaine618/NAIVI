b5bfc1850f3ae38fe9a986b0cae4cd3a202c486e
fontaine618 <simfont@umich.edu>
2024-10-30 13:31:30 -0400

    cora selection

--- experiments/cora_selection/job.sh
+++ experiments/cora_selection/job.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=naivi_cora_selection
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=10:00:00
-#SBATCH --array=0-9
+#SBATCH --time=20:00:00
+#SBATCH --array=0
 #SBATCH --account=open
 #SBATCH --partition=open
 #SBATCH --nodes=1
